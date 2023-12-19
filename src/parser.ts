8160286dc639dd6374aaccda8fd2686811165bb7
nachovigilante <nachovigilante@gmail.com>
2023-12-19 11:07:14 -0300

    feat: added LL1 parser

---
+++ src/parser.ts
@@ -0,0 +1,116 @@
+/**
+ * A parser.
+ */
+
+import { CFG, GrammarSymbol, Production } from "./cfg";
+import {
+    buildParseTree,
+    computeGuidelineSymbols,
+    computeLL1Table,
+    isLL1,
+} from "./grammarProperties";
+
+/**
+ * A parse tree.
+ */
+export type ParseTree = {
+    /**
+     * The value of the tree node.
+     */
+    value: string;
+    /**
+     * The children of the tree.
+     */
+    children: ParseTree[];
+};
+
+interface Parser {
+    /**
+     * The grammar of the parser.
+     */
+    grammar: CFG;
+    /**
+     * The parse function that takes a string and returns a parse tree.
+     */
+    parse(input: string[]): ParseTree;
+}
+
+/**
+ * An LL(1) parser.
+ * @param grammar The grammar of the parser.
+ */
+export class LL1Parser implements Parser {
+    readonly grammar: CFG;
+    readonly guidelineSymbols: Map<Production, Set<GrammarSymbol>>;
+    readonly parseTable: Map<
+        GrammarSymbol,
+        Map<GrammarSymbol, Set<Production>>
+    >;
+
+    constructor(grammar: CFG) {
+        this.grammar = grammar;
+        this.guidelineSymbols = computeGuidelineSymbols(
+            grammar.firstMap,
+            grammar.followMap,
+            grammar.productions,
+        );
+        this.parseTable = computeLL1Table(
+            grammar.nonTerminals,
+            this.guidelineSymbols,
+            grammar.productions,
+        );
+    }
+
+    parse(input: string[]): ParseTree {
+        input.push("$");
+        if (!isLL1(this.grammar)) throw new Error("Grammar is not LL(1).");
+
+        const stack: GrammarSymbol[] = ["$", this.grammar.startSymbol];
+
+        const derivation: Production[] = [];
+
+        let i = 0;
+
+        do {
+            const symbol = stack.pop()!;
+            const nextSymbol = input[0];
+
+            console.log(symbol, nextSymbol);
+
+            if (symbol === nextSymbol) {
+                input = input.slice(1);
+                continue;
+            }
+
+            if (this.grammar.terminals.has(symbol)) {
+                throw new Error(
+                    `Expected ${symbol} but got ${nextSymbol} at position ${i}`,
+                );
+            }
+
+            const row = this.parseTable.get(symbol)!;
+            const productions = row.get(nextSymbol);
+
+            if (!productions) {
+                throw new Error(`Unexpected ${nextSymbol} at position ${i}`);
+            }
+
+            const production = productions.values().next().value;
+
+            derivation.push(production);
+
+            stack.push(...production.body.reverse());
+            ++i;
+        } while (stack.length > 0);
+
+        if (input.length > 0) {
+            throw new Error(`Unexpected end of input at position ${i}`);
+        }
+
+        console.log(derivation);
+
+        const parseTree = buildParseTree(derivation);
+
+        return parseTree;
+    }
+}
--- test/grammar.test.ts
+++ test/grammar.test.ts
@@ -1,5 +1,10 @@
 import { EOF, EPSILON, CFG } from "../src/cfg";
-import { computeGuidelineSymbols } from "../src/grammarProperties";
+import {
+    computeGuidelineSymbols,
+    computeLL1Table,
+    isLL1,
+} from "../src/grammarProperties";
+import { LL1Parser } from "../src/parser";
 import { sameSet } from "../src/setOperations";
 
 const sameFunctionMap = <K, V>(map1: Map<K, Set<V>>, map2: Map<K, Set<V>>) => {
@@ -8,6 +13,47 @@ const sameFunctionMap = <K, V>(map1: Map<K, Set<V>>, map2: Map<K, Set<V>>) => {
         [...map1].every(([key, value]) => {
             const otherValue = map2.get(key)!;
             return otherValue !== undefined && sameSet(value, otherValue);
+        })
+    );
+};
+
+describe("CFG validation tests", () => {
+    it("should not allow terminals in production heads", () => {
+        const terminals = new Set(["a", "b"]);
+        const nonTerminals = new Set(["S"]);
+        const productions = [
+            { head: "S", body: ["a"] },
+            { head: "a", body: ["b"] },
+        ];
+        expect(
+            () => new CFG(nonTerminals, terminals, productions, "S"),
+        ).toThrow();
+    });
+
+    it("should ensure all production body symbols are terminals or non-terminals", () => {
+        const terminals = new Set(["a", "b"]);
+        const nonTerminals = new Set(["S"]);
+        const productions = [
+            { head: "S", body: ["a", "b"] },
+            { head: "S", body: ["b", "c"] },
+        ];
+        expect(
+            () => new CFG(nonTerminals, terminals, productions, "S"),
+        ).toThrow();
+    });
+});
+
+const same2DFunctionMap = <K1, K2, V>(
+    map1: Map<K1, Map<K2, Set<V>>>,
+    map2: Map<K1, Map<K2, Set<V>>>,
+) => {
+    return (
+        map1.size === map2.size &&
+        [...map1].every(([key, value]) => {
+            const otherValue = map2.get(key)!;
+            return (
+                otherValue !== undefined && sameFunctionMap(value, otherValue)
+            );
         })
     );
 };
@@ -149,7 +195,7 @@ describe("firstOf function tests", () => {
 });
 
 describe("FOLLOW function tests", () => {
-    it("should compute the FOLLOW for a basic grammar", () => {
+    it("should compute the FOLLOW function for a basic grammar", () => {
         const grammar = new CFG(
             new Set(["S", "A", "B"]),
             new Set(["a", "b"]),
@@ -170,7 +216,7 @@ describe("FOLLOW function tests", () => {
         expect(sameFunctionMap(grammar.followMap, expectedMap)).toBe(true);
     });
 
-    it("should compute the FOLLOW for a grammar with epsilon productions", () => {
+    it("should compute the FOLLOW function for a grammar with epsilon productions", () => {
         const grammar = new CFG(
             new Set(["S", "A", "B"]),
             new Set(["a", "b"]),
@@ -192,7 +238,7 @@ describe("FOLLOW function tests", () => {
         expect(sameFunctionMap(grammar.followMap, expectedMap)).toBe(true);
     });
 
-    it("should compute the FOLLOW for a grammar with epsilon productions and cycles", () => {
+    it("should compute the FOLLOW function for a grammar with epsilon productions and cycles", () => {
         const grammar = new CFG(
             new Set(["S", "A", "B"]),
             new Set(["a", "b"]),
@@ -229,22 +275,17 @@ describe("Guideline Symbols function tests", () => {
             "S",
         );
 
+        const parser = new LL1Parser(grammar);
+
         const expectedMap = new Map([
             [grammar.productions[0], new Set(["a"])],
             [grammar.productions[1], new Set(["a"])],
             [grammar.productions[2], new Set(["b"])],
         ]);
 
-        expect(
-            sameFunctionMap(
-                computeGuidelineSymbols(
-                    grammar.firstMap,
-                    grammar.followMap,
-                    grammar.productions,
-                ),
-                expectedMap,
-            ),
-        ).toBe(true);
+        expect(sameFunctionMap(parser.guidelineSymbols, expectedMap)).toBe(
+            true,
+        );
     });
 
     it("should compute the guideline symbols for a grammar with epsilon productions", () => {
@@ -260,6 +301,8 @@ describe("Guideline Symbols function tests", () => {
             "S",
         );
 
+        const parser = new LL1Parser(grammar);
+
         const expectedMap = new Map([
             [grammar.productions[0], new Set(["a", "b"])],
             [grammar.productions[1], new Set(["a"])],
@@ -267,16 +310,9 @@ describe("Guideline Symbols function tests", () => {
             [grammar.productions[3], new Set(["b"])],
         ]);
 
-        expect(
-            sameFunctionMap(
-                computeGuidelineSymbols(
-                    grammar.firstMap,
-                    grammar.followMap,
-                    grammar.productions,
-                ),
-                expectedMap,
-            ),
-        ).toBe(true);
+        expect(sameFunctionMap(parser.guidelineSymbols, expectedMap)).toBe(
+            true,
+        );
     });
 
     it("should compute the guideline symbols for a grammar with epsilon productions and cycles", () => {
@@ -293,6 +329,8 @@ describe("Guideline Symbols function tests", () => {
             "S",
         );
 
+        const parser = new LL1Parser(grammar);
+
         const expectedMap = new Map([
             [grammar.productions[0], new Set(["a", "b", "$"])],
             [grammar.productions[1], new Set(["a"])],
@@ -301,23 +339,208 @@ describe("Guideline Symbols function tests", () => {
             [grammar.productions[4], new Set(["$"])],
         ]);
 
-        console.log(
-            computeGuidelineSymbols(
-                grammar.firstMap,
-                grammar.followMap,
-                grammar.productions,
-            ),
+        expect(sameFunctionMap(parser.guidelineSymbols, expectedMap)).toBe(
+            true,
+        );
+    });
+});
+
+describe("LL(1) table function tests", () => {
+    it("should compute the LL(1) table for a basic grammar", () => {
+        const grammar = new CFG(
+            new Set(["S", "A", "B"]),
+            new Set(["a", "b", "c"]),
+            [
+                { head: "S", body: ["A", "B"] },
+                { head: "A", body: ["a"] },
+                { head: "B", body: ["b"] },
+            ],
+            "S",
         );
 
+        const expectedMap = new Map([
+            ["S", new Map([["a", new Set([grammar.productions[0]])]])],
+            ["A", new Map([["a", new Set([grammar.productions[1]])]])],
+            ["B", new Map([["b", new Set([grammar.productions[2]])]])],
+        ]);
+
         expect(
-            sameFunctionMap(
-                computeGuidelineSymbols(
-                    grammar.firstMap,
-                    grammar.followMap,
+            same2DFunctionMap(
+                computeLL1Table(
+                    grammar.nonTerminals,
+                    computeGuidelineSymbols(
+                        grammar.firstMap,
+                        grammar.followMap,
+                        grammar.productions,
+                    ),
                     grammar.productions,
                 ),
                 expectedMap,
             ),
         ).toBe(true);
+
+        expect(isLL1(grammar)).toBe(true);
+    });
+
+    it("should compute the LL(1) table for a grammar with epsilon productions", () => {
+        const grammar = new CFG(
+            new Set(["S", "A", "B"]),
+            new Set(["a", "b", "c"]),
+            [
+                { head: "S", body: ["A", "B"] },
+                { head: "A", body: ["a"] },
+                { head: "A", body: [EPSILON] },
+                { head: "B", body: ["b"] },
+            ],
+            "S",
+        );
+
+        const expectedMap = new Map([
+            [
+                "S",
+                new Map([
+                    ["a", new Set([grammar.productions[0]])],
+                    ["b", new Set([grammar.productions[0]])],
+                ]),
+            ],
+            [
+                "A",
+                new Map([
+                    ["a", new Set([grammar.productions[1]])],
+                    ["b", new Set([grammar.productions[2]])],
+                ]),
+            ],
+            ["B", new Map([["b", new Set([grammar.productions[3]])]])],
+        ]);
+
+        expect(
+            same2DFunctionMap(
+                computeLL1Table(
+                    grammar.nonTerminals,
+                    computeGuidelineSymbols(
+                        grammar.firstMap,
+                        grammar.followMap,
+                        grammar.productions,
+                    ),
+                    grammar.productions,
+                ),
+                expectedMap,
+            ),
+        ).toBe(true);
+
+        expect(isLL1(grammar)).toBe(true);
+    });
+
+    it("should compute the LL(1) table for a grammar with epsilon productions and cycles", () => {
+        const grammar = new CFG(
+            new Set(["S", "A", "B"]),
+            new Set(["a", "b", "c"]),
+            [
+                { head: "S", body: ["A", "B"] },
+                { head: "A", body: ["A", "a"] },
+                { head: "A", body: [EPSILON] },
+                { head: "B", body: ["b", "B"] },
+                { head: "B", body: [EPSILON] },
+            ],
+            "S",
+        );
+
+        const expectedMap = new Map([
+            [
+                "S",
+                new Map([
+                    ["a", new Set([grammar.productions[0]])],
+                    ["b", new Set([grammar.productions[0]])],
+                    ["$", new Set([grammar.productions[0]])],
+                ]),
+            ],
+            [
+                "A",
+                new Map([
+                    [
+                        "a",
+                        new Set([
+                            grammar.productions[1],
+                            grammar.productions[2],
+                        ]),
+                    ],
+                    ["b", new Set([grammar.productions[2]])],
+                    ["$", new Set([grammar.productions[2]])],
+                ]),
+            ],
+            [
+                "B",
+                new Map([
+                    ["b", new Set([grammar.productions[3]])],
+                    ["$", new Set([grammar.productions[4]])],
+                ]),
+            ],
+        ]);
+
+        expect(
+            same2DFunctionMap(
+                computeLL1Table(
+                    grammar.nonTerminals,
+                    computeGuidelineSymbols(
+                        grammar.firstMap,
+                        grammar.followMap,
+                        grammar.productions,
+                    ),
+                    grammar.productions,
+                ),
+                expectedMap,
+            ),
+        ).toBe(true);
+
+        expect(isLL1(grammar)).toBe(false);
+    });
+});
+
+describe("LL(1) parser tests", () => {
+    it("should parse a basic grammar", () => {
+        const grammar = new CFG(
+            new Set(["S", "A", "B"]),
+            new Set(["a", "b", "c"]),
+            [
+                { head: "S", body: ["A", "B"] },
+                { head: "A", body: ["a"] },
+                { head: "B", body: ["b"] },
+            ],
+            "S",
+        );
+
+        const parser = new LL1Parser(grammar);
+
+        const inputWord = ["a", "b"];
+        const expectedParseTree = {
+            value: "S",
+            children: [
+                {
+                    value: "A",
+                    children: [
+                        {
+                            value: "a",
+                            children: [],
+                        },
+                    ],
+                },
+                {
+                    value: "B",
+                    children: [
+                        {
+                            value: "b",
+                            children: [],
+                        },
+                    ],
+                },
+            ],
+        };
+
+        expect(parser.parse(inputWord)).toEqual(expectedParseTree);
+
+        const inputWord2 = ["a"];
+        expect(() => parser.parse(inputWord2)).toThrow(
+            
+        );
     });
 });
